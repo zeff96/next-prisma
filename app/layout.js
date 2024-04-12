aae4a2bbf4157cf1ae7b998b14478a7f0ee993c7
zeff96 <zeffo1975@gmail.com>
2024-04-12 22:55:20 +0300

    Add sessionProvider wrapper around all layout children

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
+import SessionProviders from "./components/sessionProvider/SessionProvider";
 import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SessionProviders>
+          <Navbar />
+          {children}
+        </SessionProviders>
+      </body>
     </html>
   );
 }
