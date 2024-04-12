a85a048d42fc4a8b689c3be2adf3b0fb154e8449
zeff96 <zeffo1975@gmail.com>
2024-04-12 23:01:37 +0300

    Add package.json update to include next-auth v5 instead of next-auth v4

--- app/page.js
+++ app/page.js
@@ -1,1 +1,16 @@
-export default async function Home() {}
+"use client";
+
+import { useSession } from "next-auth/react";
+
+export default function Home() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        <h2>{session.user.name}</h2>
+        <p>{session.user.email}</p>
+      </>
+    );
+  }
+}
