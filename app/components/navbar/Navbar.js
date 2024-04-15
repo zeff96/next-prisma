cc0e75a9efead5f97f19f0c599062416b035ddd8
zeff96 <zeffo1975@gmail.com>
2024-04-15 20:24:18 +0300

    Add wrapper button around signin button

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { SignIn } from "../signin/sign-in";
+import { ButtonWrapper } from "../buttonWrapper/button-wrapper";
 
 const Navbar = () => {
   return (
-    <header>
+    <header className="w-full bg-white p-3">
       <nav>
-        <ul>
-          <li>
-            <SignIn />
-          </li>
+        <ul className="grid grid-flow-col justify-items-center">
+          <div className="flex gap-2">
+            <li>Posts</li>
+            <li>Notifications</li>
+          </div>
+          <div className="justify-self-end">
+            <li>
+              <ButtonWrapper>
+                <button type="button">Sign in</button>
+              </ButtonWrapper>
+            </li>
+          </div>
         </ul>
       </nav>
     </header>
