30dc5f05a8e786d58f8097e0398f37f14191ac24
zeff96 <zeffo1975@gmail.com>
2024-04-16 18:47:45 +0300

    Add routes js definition for all routes within the app

---
+++ routes.js
@@ -0,0 +1,30 @@
+/**
+ * An array of routes that are public
+ * These routes do not require authentication
+ * @type {string[]}
+ */
+
+export const publicRoutes = ["/", "/notifications"];
+
+/**
+ * An array of routes used for authentication
+ * These routes will redirect logged in users to homepage
+ * @type {string []}
+ */
+
+export const authRoutes = ["/auth/login", "/auth/registration"];
+
+/**
+ * The prefix for API authentication
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type {string}
+ */
+
+export const prefixRoutes = "/api/auth";
+
+/**
+ * The default redirect after login
+ * @type {string}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/";
