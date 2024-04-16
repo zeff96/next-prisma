866cdec042d5a84a1270d3e5a306b114d5c7f721
zeff96 <zeffo1975@gmail.com>
2024-04-16 18:50:38 +0300

    Add registration component for user registration

---
+++ app/auth/registration/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SignupForm } from "@/app/components/auth/register/register-form";
+
+const Registration = () => {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
+};
+
+export default Registration;
