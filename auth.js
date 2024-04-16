24db91b53d65e75cf4e535ce6c89a249c221a2b1
zeff96 <zeffo1975@gmail.com>
2024-04-16 18:46:50 +0300

    Add session and jwt callbacks to manage user sessions

--- auth.js
+++ auth.js
@@ -1,9 +1,36 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./lib/prisma/prisma";
-import Google from "next-auth/providers/google";
+import authConfig from "./auth.config";
+import { getUserById } from "./data/user";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+  },
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
