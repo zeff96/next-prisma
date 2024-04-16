c0390d8652b4b39955cdac4b8cafef480b0a18f8
zeff96 <zeffo1975@gmail.com>
2024-04-16 18:49:52 +0300

    Add login page component for user authentication

---
+++ app/auth/login/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { LoginForm } from "../../components/auth/login/login-form";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
