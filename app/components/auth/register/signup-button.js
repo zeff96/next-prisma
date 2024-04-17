755ceaea9b01032137b15af5e5f8c850b477ac9f
zeff96 <zeffo1975@gmail.com>
2024-04-17 17:18:22 +0300

    Rename sign up button from login to signup

--- app/components/auth/register/signup-button.js
+++ app/components/auth/register/signup-button.js
@@ -11,7 +11,7 @@ export const RegistrationButton = () => {
       aria-disabled={pending}
       className="w-full bg-zinc-900 py-2 px-4 text-white rounded-lg hover:bg hover:bg-zinc-500 focus:outline-none"
     >
-      Login
+      Signup
     </button>
   );
 };
