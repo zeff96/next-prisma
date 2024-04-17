e8c44b23e4d19c8f25d868020daf48d571af039d
zeff96 <zeffo1975@gmail.com>
2024-04-17 17:15:49 +0300

    Add Oauth providers login

--- auth.config.js
+++ auth.config.js
@@ -1,11 +1,14 @@
 import bcrypt from "bcryptjs";
 import Credentials from "next-auth/providers/credentials";
-import google from "next-auth/providers/google";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
 import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
 
 export default {
   providers: [
+    Google,
+    Github,
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
