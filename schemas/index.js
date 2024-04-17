c5495206d2f4fc41ae1e5eba4a101d37a5d236ee
zeff96 <zeffo1975@gmail.com>
2024-04-17 17:20:02 +0300

    Add zod validations for all registartion fields

--- schemas/index.js
+++ schemas/index.js
@@ -1,24 +1,46 @@
 import { z } from "zod";
 
 export const LoginSchema = z.object({
-  email: z.string({ invalid_type_error: "Invalid email" }).email({
-    message: "Email required!",
-  }),
-  password: z.string({
-    invalid_type_error: "Invalid password!",
-  }),
-});
-
-export const RegisterSchema = z.object({
-  name: z.string({
-    invalid_type_error: "Invalid username!",
-  }),
-  email: z.string({
-    invalid_type_error: "Invalid email!",
-  }),
+  email: z
+    .string({ invalid_type_error: "Invalid email" })
+    .email({
+      message: "Email required!",
+    })
+    .trim(),
   password: z
     .string({
       invalid_type_error: "Invalid password!",
     })
-    .min(6),
+    .trim(),
+});
+
+export const RegisterSchema = z.object({
+  name: z
+    .string({
+      invalid_type_error: "Invalid username!",
+    })
+    .min(2, { message: "Name must be atleast 2 characters long!" })
+    .trim(),
+  email: z
+    .string({
+      invalid_type_error: "Invalid email!",
+    })
+    .email({ message: "Please enter valid email!" })
+    .trim(),
+  password: z
+    .string({
+      invalid_type_error: "Invalid password!",
+    })
+    .min(6, { message: "Password must be atleast 6 characters long!" })
+    .regex(/[A-Z]/, {
+      message: "Password must contain atleast one uppercase letter!",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain atleas one lowercase letter!",
+    })
+    .regex(/[0-9]/, { message: "Password must contain atleast one number" })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: "Password must contain atleast one special character!",
+    })
+    .trim(),
 });
