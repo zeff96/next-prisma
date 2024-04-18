e8c0183e1f7e3359bc60c14bf9dfde3e2f3b9705
zeff96 <zeffo1975@gmail.com>
2024-04-18 22:38:41 +0300

    Add nodemailer to allow for sending of user emails

---
+++ data/mailer.js
@@ -0,0 +1,28 @@
+import nodemailer from "nodemailer";
+
+const user = process.env.USER_MAIL;
+const pass = process.env.USER_PASSWORD;
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user,
+    pass,
+    method: "PLAIN",
+  },
+});
+
+export const sendVerificationEmail = async (email, token) => {
+  const emailLink = `http://localhost:3000/auth/email-verification?token=${token}`;
+
+  try {
+    await transporter.sendMail({
+      from: user,
+      to: email,
+      subject: "Account email confirmation",
+      html: `<p>Click <a href="${emailLink}">here</a> to confirm your account</p>`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
