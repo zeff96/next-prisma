8eeec6f44099c33c0b3ed5466a99ef6f28b776ea
zeff96 <zeffo1975@gmail.com>
2024-04-18 22:39:35 +0300

    Add methods to retrieve token from db by use of email and token

---
+++ data/verification_token.js
@@ -0,0 +1,29 @@
+import prisma from "@/lib/prisma/prisma";
+
+export const getVerificationTokenByEmail = async (email) => {
+  try {
+    const verificationToken = await prisma.verificationToken.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    return verificationToken;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const getVerificationTokenToken = async (token) => {
+  try {
+    const verificationToken = await prisma.verificationToken.findUnique({
+      where: {
+        token,
+      },
+    });
+
+    return verificationToken;
+  } catch (error) {
+    return null;
+  }
+};
