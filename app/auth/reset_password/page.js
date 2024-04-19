d82be323af2f980ca54b0bd87a2fd6e3e2d06ae2
zeff96 <zeffo1975@gmail.com>
2024-04-19 15:22:41 +0300

    Add router page for password reset

---
+++ app/auth/reset_password/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ResetPassword } from "@/app/components/auth/reset_password/reset_password";
+
+const Page = () => {
+  return <ResetPassword />;
+};
+
+export default Page;
