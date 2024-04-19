508fff18d0379fa4cf8aad4147f41768d0e6188c
zeff96 <zeffo1975@gmail.com>
2024-04-19 15:24:30 +0300

    Add password reset form component

---
+++ app/components/auth/reset_password/reset_password.js
@@ -0,0 +1,52 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { useFormState } from "react-dom";
+import { ResetPasswordButton } from "./reset_password_button";
+import { resetPassword } from "@/actions/reset_password";
+
+export const ResetPassword = () => {
+  const [state, action] = useFormState(resetPassword, undefined);
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-gray-100">
+      <form
+        action={action}
+        className="w-[400px] bg-white flex flex-col space-y-4 p-4 rounded-lg shadow-md "
+      >
+        <h3 className="text-center">Reset password</h3>
+        <div>
+          <label htmlFor="email">
+            Email
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="doe@example.com"
+              className="w-full block border rounded-lg focus:outline-none focus:ring focus:ring-gray-300 py-2 px-4"
+            />
+          </label>
+        </div>
+        <div>
+          {state?.errors && (
+            <p className="w-full py-1 px-4 text-white bg-red-500 rounded-lg">
+              {state?.errors}
+            </p>
+          )}
+          {state?.message && (
+            <p className="w-full py-1 px-4 text-white bg-green-500 rounded-lg">
+              {state?.message}
+            </p>
+          )}
+        </div>
+        <div>
+          <ResetPasswordButton />
+        </div>
+        <hr />
+        <div className="text-center underline">
+          <Link href="/auth/login">Back to login page</Link>
+        </div>
+      </form>
+    </div>
+  );
+};
