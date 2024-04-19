001be66ec7bd25ac7ad39a306586ee8cbae900b2
zeff96 <zeffo1975@gmail.com>
2024-04-19 15:22:05 +0300

    Add router page for password verification

---
+++ app/auth/edit_password/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { PasswordVerification } from "@/app/components/auth/verification/password_verification";
+
+const Page = () => {
+  return <PasswordVerification />;
+};
+
+export default Page;
