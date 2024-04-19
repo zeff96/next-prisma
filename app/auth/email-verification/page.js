676f8bb63432dffddaf6fb522307f1e252f2d836
zeff96 <zeffo1975@gmail.com>
2024-04-19 15:22:24 +0300

    Add router page for email verification

---
+++ app/auth/email-verification/page.js
@@ -0,0 +1,8 @@
+import { EmailVerificationForm } from "@/app/components/auth/verification/email-verification";
+import React from "react";
+
+const EmailVerification = () => {
+  return <EmailVerificationForm />;
+};
+
+export default EmailVerification;
