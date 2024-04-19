442af0982ec81dfd6973479adc9059ae9b34db21
zeff96 <zeffo1975@gmail.com>
2024-04-19 15:24:12 +0300

    Add password reset button component

---
+++ app/components/auth/reset_password/reset_password_button.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useFormStatus } from "react-dom";
+
+export const ResetPasswordButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      type="submit"
+      aria-disabled={pending}
+      className="w-full bg-zinc-900 py-2 px-4 text-white rounded-lg hover:bg hover:bg-zinc-500 focus:outline-none"
+    >
+      reset password
+    </button>
+  );
+};
