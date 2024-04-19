f9b0e741e61b89f4059b1c505222978beeed5d2c
zeff96 <zeffo1975@gmail.com>
2024-04-19 15:21:46 +0300

    Add router page for confirmation token

---
+++ app/auth/confirmation/page.js
@@ -0,0 +1,8 @@
+import { ConfirmationToken } from "@/app/components/auth/confirmation/confirmation_token";
+import React from "react";
+
+const page = () => {
+  return <ConfirmationToken />;
+};
+
+export default page;
