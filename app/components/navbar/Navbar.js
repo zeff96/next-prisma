6c2983373021219db022b8a37d4026a5f4c5b290
zeff96 <zeffo1975@gmail.com>
2024-04-20 11:31:24 +0300

    Add links to notifications and homepage respectively

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -1,21 +1,37 @@
 import React from "react";
-import { ButtonWrapper } from "../buttonWrapper/button-wrapper";
+import Link from "next/link";
 
-const Navbar = () => {
+import { ButtonWrapper } from "../buttonWrapper/button-wrapper";
+import { SignOutButton } from "../buttonWrapper/signout-wrapper";
+import { auth } from "@/auth";
+
+const Navbar = async () => {
+  const session = await auth();
+
   return (
     <header className="w-full bg-white p-3">
       <nav>
         <ul className="grid grid-flow-col justify-items-center">
           <div className="flex gap-2">
-            <li>Posts</li>
-            <li>Notifications</li>
+            <li>
+              <Link href="/">Posts</Link>
+            </li>
+            <li>
+              <Link href="/notifications">Notifications</Link>
+            </li>
           </div>
           <div className="justify-self-end">
-            <li>
-              <ButtonWrapper>
-                <button type="button">Sign in</button>
-              </ButtonWrapper>
-            </li>
+            {session ? (
+              <li>
+                <SignOutButton />
+              </li>
+            ) : (
+              <li>
+                <ButtonWrapper>
+                  <button type="button">Sign in</button>
+                </ButtonWrapper>
+              </li>
+            )}
           </div>
         </ul>
       </nav>
