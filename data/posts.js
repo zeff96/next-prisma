81a8ac221c238bc6e0bd1338c6210cd6e7f413c3
zeff96 <zeffo1975@gmail.com>
2024-04-25 23:00:28 +0300

    Add query to include author for each post on post query

--- data/posts.js
+++ data/posts.js
@@ -2,7 +2,11 @@ import prisma from "@/lib/prisma/prisma";
 
 export const getAllPosts = async () => {
   try {
-    const posts = await prisma.post.findMany();
+    const posts = await prisma.post.findMany({
+      include: {
+        user: true,
+      },
+    });
     return posts;
   } catch (error) {
     console.log(`Something went wrong, ${error.message}`);
