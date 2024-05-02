c2c10c96ad3ba1b7f0a65daba7c2eab81d09aedd
zeff96 <zeffo1975@gmail.com>
2024-05-02 16:02:12 +0300

    Remove background color

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ import { auth } from "@/auth";
 export default async function Home() {
   const session = await auth();
   return (
-    <main className="w-full min-h-screen flex flex-col items-center py-5 bg-gray-100">
-      <div className="w-1/2 bg-white rounded-lg  p-3">
+    <main className="w-full min-h-screen flex flex-col items-center py-5">
+      <div className="w-1/2 rounded-lg  p-3">
         {session && (
           <CreatePostButtonWrapper>
             <input
