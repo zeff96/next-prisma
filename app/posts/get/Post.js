0cb95e91acbfae865de9e825fca0bb037655c4cc
zeff96 <zeffo1975@gmail.com>
2024-05-03 16:40:46 +0300

    Add post sorting based on the most recent created post

--- app/posts/get/Post.js
+++ app/posts/get/Post.js
@@ -1,6 +1,11 @@
-import { getAllPosts } from "@/data/posts";
 import React from "react";
+import Image from "next/image";
+import { FaUserCircle } from "react-icons/fa";
+
+import { getAllPosts } from "@/data/posts";
 import Social from "./social/Social";
+import { TimeAgo } from "@/app/components/timeAgo/TimeAgo";
+import { compareDesc } from "date-fns";
 
 export const Posts = async () => {
   const posts = await getAllPosts();
@@ -9,13 +14,34 @@ export const Posts = async () => {
     return <h2>No posts found!</h2>;
   }
 
+  const orderedPosts = posts.sort((a, b) =>
+    compareDesc(new Date(a.created_at), new Date(b.created_at))
+  );
+
   return (
     <>
-      {posts.map((post) => (
+      {orderedPosts.map((post) => (
         <div
           key={post.id}
           className="border border-solid border-gray-300 rounded-2xl p-3 mb-3"
         >
+          <div className="flex items-center gap-x-2 mb-3">
+            {post.user.image ? (
+              <Image
+                src={post.user.image}
+                alt={post.user.name}
+                width={50}
+                height={50}
+                style={{ borderRadius: "50%" }}
+              />
+            ) : (
+              <FaUserCircle className="text-3xl" />
+            )}
+            <div>
+              <span className="block font-bold">{post.user.name}</span>
+              <TimeAgo timestamp={post.created_at} />
+            </div>
+          </div>
           <p>{post.body}</p>
           <hr className="my-3" />
           <Social />
