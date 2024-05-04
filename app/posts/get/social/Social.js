6b10ea158a179a5c177e6aa6fdcd91a9edd3a67a
zeff96 <zeffo1975@gmail.com>
2024-05-04 11:57:02 +0300

    Add call for likes creation form to the social component

--- app/posts/get/social/Social.js
+++ app/posts/get/social/Social.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { BiLike } from "react-icons/bi";
 import { FaRegCommentAlt } from "react-icons/fa";
+import CreateLikesForm from "@/app/components/likes/create_likes";
 
-const Social = () => {
+const Social = ({ userId, postId, username }) => {
   return (
     <div className="flex justify-between">
-      <button type="button">
-        <BiLike className="inline mx-2 text-xl" />
-        Like
-      </button>
+      <CreateLikesForm userId={userId} postId={postId} username={username} />
       <button type="button">
         <FaRegCommentAlt className="inline mx-2" />
         Comment
