89b53ce72b41d803f2bc418de93a0f0a596bced7
zeff96 <zeffo1975@gmail.com>
2024-05-04 11:57:48 +0300

    Add display flex and center notification items on the center of the page

--- app/components/notifications/notifications.js
+++ app/components/notifications/notifications.js
@@ -4,8 +4,8 @@ export const Notifications = async ({ userId }) => {
   const notifications = await getNotifications(userId);
 
   return (
-    <div className="w-full h-screen flex flex-col items-center bg-gray-100">
-      <ul className="border rounded-lg bg-white">
+    <div className="w-full h-screen flex flex-col items-center">
+      <ul className="border rounded-lg">
         {notifications.map((item) => (
           <li key={item.id}>{item.content}</li>
         ))}
