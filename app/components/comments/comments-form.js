81d38af8e17c86623ff925cf16c489253d421918
zeff96 <zeffo1975@gmail.com>
2024-05-08 12:33:59 +0300

    Add background color to comments form

--- app/components/comments/comments-form.js
+++ app/components/comments/comments-form.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { useFormState } from "react-dom";
 
@@ -7,25 +9,25 @@ import { CommentsButton } from "./comments-button";
 export const CommentsForm = ({ userId, postId, username }) => {
   const [state, action] = useFormState(createComment, undefined);
   return (
-    <form action={action}>
+    <form action={action} className="flex-1 z-100">
       <input type="hidden" name="userId" value={userId} />
       <input type="hidden" name="postId" value={postId} />
       <input type="hidden" name="username" value={username} />
-      <label htmlFor="body">
-        Comment
+      <div className="relative">
         <textarea
           name="body"
           id="body"
-          placeholder="Your comment"
+          placeholder="Write a public comment..."
           required
+          className="w-full bg-zinc-500 block rounded-2xl focus:outline-none py-2 px-4 resize-none overflow-y-auto"
         ></textarea>
-      </label>
+        <CommentsButton />
+      </div>
       {state?.errors && (
         <p className="w-full py-1 px-4 text-white bg-red-500 rounded-lg">
           {state?.errors}
         </p>
       )}
-      <CommentsButton />
     </form>
   );
 };
