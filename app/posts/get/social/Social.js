dce507363500c4bbce48371f35ae573c5d24b653
zeff96 <zeffo1975@gmail.com>
2024-05-08 12:37:02 +0300

    Add display flex to justify contents between

--- app/posts/get/social/Social.js
+++ app/posts/get/social/Social.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { FaRegCommentAlt } from "react-icons/fa";
 import CreateLikesForm from "@/app/components/likes/create_likes";
+import { CommentButtonWrapper } from "@/app/components/buttonWrapper/comment-button-wrapper";
 
-const Social = ({ userId, postId, username }) => {
+export const Social = ({ userId, postId, username }) => {
   return (
     <div className="flex justify-between">
-      <CreateLikesForm userId={userId} postId={postId} username={username} />
-      <button type="button">
-        <FaRegCommentAlt className="inline mx-2" />
-        Comment
-      </button>
+      <div>
+        <CreateLikesForm userId={userId} postId={postId} username={username} />
+      </div>
+      <div>
+        <CommentButtonWrapper postId={postId}>
+          <button type="button">
+            <FaRegCommentAlt className="inline mx-2" />
+            Comment
+          </button>
+        </CommentButtonWrapper>
+      </div>
     </div>
   );
 };
-
-export default Social;
