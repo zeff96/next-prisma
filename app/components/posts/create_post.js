70f46819c3a60596f89a5c50357f8a39b3c3da75
zeff96 <zeffo1975@gmail.com>
2024-05-08 12:35:22 +0300

    Add border radius to create post form

--- app/components/posts/create_post.js
+++ app/components/posts/create_post.js
@@ -13,7 +13,7 @@ export const CreatePostForm = ({ userId, username }) => {
     <div className="w-full h-screen flex items-center justify-center">
       <form
         action={formAction}
-        className="w-[400px] flex flex-col space-y-4 p-4 rounded-lg shadow-md "
+        className="w-1/2 h-1/2 flex flex-col space-y-4 p-4 rounded-2xl shadow-md border border-gray-200"
       >
         <h3 className="text-center">Add post</h3>
         <div>
