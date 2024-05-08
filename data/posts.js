398000a87f640935415c9d1268f01cd342bad4b8
zeff96 <zeffo1975@gmail.com>
2024-05-08 12:37:34 +0300

    Add prisma query to retrieve given post from db by id

--- data/posts.js
+++ data/posts.js
@@ -4,13 +4,36 @@ export const getAllPosts = async () => {
   try {
     const posts = await prisma.post.findMany({
       include: {
-        user: true,
+        // omit user password
+        user: {
+          select: { name: true, email: true, image: true },
+        },
         _count: {
-          select: { likes: true },
+          select: { likes: true, comments: true },
         },
       },
     });
     return posts;
+  } catch (error) {
+    console.log(`Something went wrong, ${error.message}`);
+  }
+};
+
+export const getPostById = async (id) => {
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id },
+      include: {
+        // omit user password
+        user: {
+          select: { name: true, email: true, image: true },
+        },
+        _count: {
+          select: { likes: true, comments: true },
+        },
+      },
+    });
+    return post;
   } catch (error) {
     console.log(`Something went wrong, ${error.message}`);
   }
