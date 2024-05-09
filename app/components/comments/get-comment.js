61b8923e11f3c8664541dc266a8f20d1272e4a38
zeff96 <zeffo1975@gmail.com>
2024-05-09 12:58:59 +0300

    Add component to render comment on the page

---
+++ app/components/comments/get-comment.js
@@ -0,0 +1,20 @@
+import { TimeAgo } from "../timeAgo/TimeAgo";
+
+export const Comment = ({ comment }) => {
+  return (
+    <div className="flex items-start space-x-2">
+      <img
+        src={comment.user.image}
+        alt="avatar"
+        className="w-8 h-8 rounded-full"
+      />
+      <div>
+        <div className="bg-zinc-500 flex flex-col rounded-2xl py-2 px-4">
+          <h4 className="font-semibold text-gray-100">{comment.user.name}</h4>
+          <p className="text-sm text-gray-100">{comment.body}</p>
+        </div>
+        <TimeAgo timestamp={comment.created_at} />
+      </div>
+    </div>
+  );
+};
