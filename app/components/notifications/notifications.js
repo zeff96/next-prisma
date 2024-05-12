09f9bb026c11fbdbb63fc5ce78d4c12b9a09c62e
zeff96 <zeffo1975@gmail.com>
2024-05-12 16:42:58 +0300

    Add render for notifications on the page

--- app/components/notifications/notifications.js
+++ app/components/notifications/notifications.js
@@ -1,15 +1,40 @@
 import { getNotifications } from "@/lib/notifications";
+import { compareDesc } from "date-fns";
 
 export const Notifications = async ({ userId }) => {
   const notifications = await getNotifications(userId);
 
-  return (
-    <div className="w-full h-screen flex flex-col items-center">
-      <ul className="border rounded-lg">
-        {notifications.map((item) => (
-          <li key={item.id}>{item.content}</li>
-        ))}
-      </ul>
-    </div>
+  const orderedNotifications = notifications.sort((a, b) =>
+    compareDesc(new Date(a.createdAt), new Date(b.createdAt))
   );
+
+  const listNotifications = orderedNotifications.map((notification) => {
+    console.log(notification?.post);
+    if (notification.type === "like") {
+      return (
+        <div key={notification.id} className="notification">
+          <img src={notification.user.image} alt={notification.user.name} />
+          <p>{notification.user.name} liked your post</p>
+        </div>
+      );
+    } else if (notification.type === "comment") {
+      return (
+        <div key={notification.id} className="notification">
+          <img src={notification.user.image} alt={notification.user.name} />
+          <p>{notification.user.name} commented on your post</p>
+        </div>
+      );
+    } else if (notification.type === "post") {
+      return (
+        <div key={notification.id} className="notification">
+          <img src={notification.user.image} alt={notification.user.name} />
+          <p>
+            {notification.user.name} posted: {notification?.post?.body}
+          </p>
+        </div>
+      );
+    }
+  });
+
+  return <div>{listNotifications}</div>;
 };
