b4871372910cbd016cfa9a85b55e315d871f8fc5
zeff96 <zeffo1975@gmail.com>
2024-05-13 10:39:29 +0300

    Remove username variable from likes creation method

--- app/components/likes/create_likes.js
+++ app/components/likes/create_likes.js
@@ -6,13 +6,12 @@ import { useFormState } from "react-dom";
 import { BiLike } from "react-icons/bi";
 import { createLike } from "@/actions/likes";
 
-const CreateLikesForm = ({ userId, postId, username }) => {
+const CreateLikesForm = ({ userId, postId }) => {
   const [state, action] = useFormState(createLike, undefined);
   return (
     <form action={action}>
       <input type="hidden" name="userId" value={userId} />
       <input type="hidden" name="postId" value={postId} />
-      <input type="hidden" name="username" value={username} />
       {state?.errors && (
         <p className="w-full py-1 px-4 text-white bg-red-500 rounded-lg">
           {state?.errors}
