0b36614a41a7c4def82884244de77cf37c94702a
zeff96 <zeffo1975@gmail.com>
2024-05-14 17:15:58 +0300

    Add sizing for user avatar

--- app/components/notifications/notifications.js
+++ app/components/notifications/notifications.js
@@ -1,5 +1,6 @@
 import { getNotifications } from "@/lib/notifications";
 import { compareDesc } from "date-fns";
+import Image from "next/image";
 
 export const Notifications = async ({ userId }) => {
   const notifications = await getNotifications(userId);
@@ -11,13 +12,17 @@ export const Notifications = async ({ userId }) => {
   const listNotifications = orderedNotifications.map((notification) => {
     if (notification.type === "like") {
       return (
-        <div key={notification.id} className="notification">
-          <img
+        <div key={notification.id} className="flex gap-x-2 items-center">
+          <Image
             src={notification.post.user.image}
             alt={notification.post.user.name}
+            width={40}
+            height={40}
+            style={{ borderRadius: "50%" }}
           />
           <p>
-            {notification.post.user.name} liked a post:{" "}
+            <span className="font-bold">{notification.post.user.name}</span>{" "}
+            liked a post:{" "}
             {notification.post.body.length > 100
               ? `${notification.post.body.slice(0, 100)}...`
               : notification.post.body}
@@ -26,13 +31,17 @@ export const Notifications = async ({ userId }) => {
       );
     } else if (notification.type === "comment") {
       return (
-        <div key={notification.id} className="notification">
-          <img
+        <div key={notification.id} className="flex gap-x-2 items-center">
+          <Image
             src={notification.post.user.image}
             alt={notification.post.user.name}
+            width={40}
+            height={40}
+            style={{ borderRadius: "50%" }}
           />
           <p>
-            {notification.post.user.name} commented on a post:{" "}
+            <span className="font-bold">{notification.post.user.name}</span>{" "}
+            commented on a post:{" "}
             {notification.post.body.length > 100
               ? `${notification.post.body.slice(0, 100)}...`
               : notification.post.body}
@@ -41,13 +50,17 @@ export const Notifications = async ({ userId }) => {
       );
     } else if (notification.type === "post") {
       return (
-        <div key={notification.id} className="notification">
-          <img
+        <div key={notification.id} className="flex gap-x-2 items-center">
+          <Image
             src={notification.post.user.image}
             alt={notification.post.user.name}
+            width={40}
+            height={40}
+            style={{ borderRadius: "50%" }}
           />
           <p>
-            {notification.post.user.name} posted:{" "}
+            <span className="font-bold">{notification.post.user.name}</span>{" "}
+            posted:{" "}
             {notification.post.body.length > 100
               ? `${notification.post.body.slice(0, 100)}...`
               : notification.post.body}
@@ -59,7 +72,7 @@ export const Notifications = async ({ userId }) => {
 
   return (
     <div className="w-full h-screen flex flex-col items-center py-3">
-      <div className="items-start">{listNotifications}</div>
+      <div className="w-1/3 items-start">{listNotifications}</div>
     </div>
   );
 };
