297e0526e715b2552e73d020f87e1c56a6adb24e
zeff96 <zeffo1975@gmail.com>
2024-05-14 17:15:09 +0300

    Add default avatar when user image not available

--- app/components/comments/get-comment.js
+++ app/components/comments/get-comment.js
@@ -1,13 +1,21 @@
 import { TimeAgo } from "../timeAgo/TimeAgo";
+import Image from "next/image";
+import { FaUserCircle } from "react-icons/fa";
 
 export const Comment = ({ comment }) => {
   return (
     <div className="flex items-start space-x-2">
-      <img
-        src={comment.user.image}
-        alt="avatar"
-        className="w-8 h-8 rounded-full"
-      />
+      {comment.user.image ? (
+        <Image
+          src={comment.user.image}
+          alt={comment.user.name}
+          width={40}
+          height={40}
+          style={{ borderRadius: "50%" }}
+        />
+      ) : (
+        <FaUserCircle className="text-3xl" />
+      )}
       <div>
         <div className="bg-zinc-500 flex flex-col rounded-2xl py-2 px-4">
           <h4 className="font-semibold text-gray-100">{comment.user.name}</h4>
