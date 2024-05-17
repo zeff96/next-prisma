0cca024555cde7286c948a32fa421d988a5b492a
zeff96 <zeffo1975@gmail.com>
2024-05-17 11:44:48 +0300

    Add border top to first list child and border bottom to last list child

--- app/components/notifications/notifications.js
+++ app/components/notifications/notifications.js
@@ -12,36 +12,44 @@ export const Notifications = async ({ userId }) => {
     compareDesc(new Date(a.dateCreated), new Date(b.dateCreated))
   );
 
-  const listNotifications = orderedNotifications.map((notification) => (
+  const listNotifications = orderedNotifications.map((notification, index) => (
     <NotificationButtonWrapper
       id={notification.id}
       postId={notification.postId}
       key={notification.id}
     >
       <li
-        className={`flex gap-x-2 items-center ${
-          notification.read ? "" : "bg-teal-500"
-        } p-3 rounded-b-2xl`}
+        className={`flex gap-x-3 items-center p-3 border-l border-r border-gray-700 ${
+          notification.read ? "" : "bg-blue-300"
+        } ${index === 0 ? "rounded-t-2xl border-t" : ""} ${
+          index === orderedNotifications.length - 1
+            ? "rounded-b-2xl border-b"
+            : ""
+        }`}
       >
-        {notification.post.user.image ? (
-          <Image
-            src={notification.post.user.image}
-            alt={notification.post.user.name}
-            width={40}
-            height={40}
-            style={{ borderRadius: "50%" }}
-          />
-        ) : (
-          <FaUserCircle className="w-[100px] h-[100px]" />
-        )}
-        <NotificationItem notification={notification} />
+        <div>
+          {notification.post.user.image ? (
+            <Image
+              src={notification.post.user.image}
+              alt={notification.post.user.name}
+              width={70}
+              height={70}
+              style={{ borderRadius: "50%" }}
+            />
+          ) : (
+            <FaUserCircle className="w-[40px] h-[40px]" />
+          )}
+        </div>
+        <div>
+          <NotificationItem notification={notification} />
+        </div>
       </li>
     </NotificationButtonWrapper>
   ));
 
   return (
     <div className="w-full h-screen flex flex-col items-center py-3">
-      <ul className="w-1/3 items-start">{listNotifications}</ul>
+      <ul className="w-2/5 items-start list-none">{listNotifications}</ul>
     </div>
   );
 };
