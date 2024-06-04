2e763a8d074d5e4d9f24fd74cfe51a1f3c306c17
zeff96 <zeffo1975@gmail.com>
2024-06-04 21:34:30 +0300

    Remove comments from the project

--- app/components/inputs/ChatInput.js
+++ app/components/inputs/ChatInput.js
@@ -7,14 +7,6 @@ import {
   IoMdSend,
 } from "react-icons/bi";
 
-/*
-Create a ChatInput component that has a textarea input field and three icons for image upload, voice recording, and sending a message. The textarea should adjust its height based on the content inside it. The component should have the following features:
-Send icon should be disabled when the textarea is empty.
-Send icon should be enabled when the textarea has some content.
-The textarea should adjust its height based on the content inside it.
-Send icon should fire request to send the message when clicked.
-*/
-
 export const ChatInput = () => {
   const [text, setText] = useState("");
   const [textAreaHeight, setTextAreaHeight] = useState("auto");
