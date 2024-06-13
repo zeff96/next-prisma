094092347ed992db54a0acf0ac093b3dcb6ac82a
zeff96 <zeffo1975@gmail.com>
2024-06-13 18:43:52 +0300

    Add toast for error and success messages

--- app/components/auth/verification/email-verification.js
+++ app/components/auth/verification/email-verification.js
@@ -3,21 +3,27 @@
 import React from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import { useFormState } from "react-dom";
 
 import { EmailVerificationButton } from "./verification-button";
 import { verifyEmail } from "@/actions/email-verification";
+import toast from "react-hot-toast";
 
 export const EmailVerificationForm = () => {
-  const [state, action] = useFormState(verifyEmail, undefined);
-
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
 
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <form
-        action={action}
+        action={async (formData) => {
+          const res = await verifyEmail(formData);
+
+          if (res.message) {
+            toast.success(res.message);
+          } else {
+            toast.error(res.errors);
+          }
+        }}
         className="w-[400px] flex flex-col space-y-4 p-4 rounded-lg shadow-md "
       >
         <h3 className="text-center">Verify email account</h3>
