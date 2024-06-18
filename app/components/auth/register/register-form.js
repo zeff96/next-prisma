dea1420a76694e6d8e2490c8c6c9f0fa10e87412
zeff96 <zeffo1975@gmail.com>
2024-06-18 21:30:49 +0300

    Remove unused error-success state management

--- app/components/auth/register/register-form.js
+++ app/components/auth/register/register-form.js
@@ -62,18 +62,6 @@ export const SignupForm = () => {
           </label>
         </div>
         <div>
-          {state?.errors && (
-            <p className="w-full py-1 px-4 text-white bg-red-500 rounded-lg">
-              {state?.errors}
-            </p>
-          )}
-          {state?.message && (
-            <p className="w-full py-1 px-4 text-white bg-green-500 rounded-lg">
-              {state?.message}
-            </p>
-          )}
-        </div>
-        <div>
           <RegistrationButton />
         </div>
         <hr />
